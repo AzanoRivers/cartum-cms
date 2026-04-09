acfeb07b207dc1accf62333da0835315572760bd
AzanoRivers <job.devcode@gmail.com>
2026-04-08 23:53:46 -0500

    fix dizzie migration fail vercel

--- db/migrations/0007_role_permissions_unique.sql
+++ db/migrations/0007_role_permissions_unique.sql
@@ -1,1 +1,10 @@
-ALTER TABLE "role_permissions" ADD CONSTRAINT "role_permissions_role_id_node_id_unique" UNIQUE("role_id","node_id");
+DO $$ BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM pg_constraint
+    WHERE conname = 'role_permissions_role_id_node_id_unique'
+  ) THEN
+    ALTER TABLE "role_permissions"
+    ADD CONSTRAINT "role_permissions_role_id_node_id_unique"
+    UNIQUE("role_id","node_id");
+  END IF;
+END $$;
